909b23943ba33780f40badf5fb15e702bda38ace
Dustin Sampson <gridcell@gmail.com>
2020-06-15 13:16:36 -0700

    supply data for species report chart

--- src/api/reports/species_report.py
+++ src/api/reports/species_report.py
@@ -1,23 +1,29 @@
 import shutil
 import tempfile
 import uuid
-
+from collections.abc import Sequence
 from pathlib import Path
 
 from django.conf import settings
 from openpyxl import load_workbook
-from openpyxl.utils import rows_from_range
-
+from openpyxl.utils import coordinate_to_tuple, get_column_letter, rows_from_range
 
 TEMPLATE_PATH = f"{settings.BASE_DIR}/templates/scl_report_mvp.xlsx"
 
 DATA_CELL_MAP = {
-    "species": "B3",
-    "report_date": "B4",
-    "country": "B5",
-    "total_protected": "D12",
-    "table_data": "B8:D11",  # 2D list (shape: 4 x 3)
+    "country summary": {
+        "species": "B3",
+        "report_date": "B4",
+        "country": "B5",
+        "total_protected": "D12",
+        "table_data": "B8:D11",  # 2D list (shape: 4 x 3)
+    },
+    "landscapes over time": {"chart_data": "A2",},
 }
+
+
+def is_sequence(val):
+    return isinstance(val, Sequence) and not isinstance(val, str)
 
 
 def open_new_template(template):
@@ -28,8 +34,28 @@ def open_new_template(template):
     return tmp_file_path
 
 
+def rows_from_data(coord, data):
+    p0, p1 = coordinate_to_tuple(coord)
+    if not data:
+        return []
+
+    if is_sequence(data[0]):
+        w = len(data[0])
+    else:
+        w = 1
+
+    start_letter = get_column_letter(p1)
+    end_letter = get_column_letter(p1 + w - 1)
+
+    return rows_from_range(f"{start_letter}{p0}:{end_letter}{p0 + h - 1}")
+
+
 def insert_range(sheet, range_str, data):
-    cells = rows_from_range(range_str)
+    if ":" in range_str:
+        cells = rows_from_range(range_str)
+    else:
+        cells = rows_from_data(range_str, data)
+
     for y, rows in enumerate(cells):
         for x, col in enumerate(rows):
             sheet[col] = data[y][x]
@@ -40,15 +66,21 @@ def generate(data):
     workbook = load_workbook(filename=report_path)
     sheet = workbook.active
 
-    for key, value in data.items():
-        coord = DATA_CELL_MAP.get(key)
-        if not coord:
-            continue
+    for sheet_name, sheet_data in data.items():
+        sheet = workbook[sheet_name]
+        sheet_map = DATA_CELL_MAP.get(sheet_name)
+        if sheet_map is None:
+            break
 
-        if ":" in coord:
-            insert_range(sheet, coord, value)
-        else:
-            sheet[coord] = value
+        for key, value in sheet_data.items():
+            coord = sheet_map.get(key)
+            if not coord:
+                continue
+
+            if is_sequence(value):
+                insert_range(sheet, coord, value)
+            else:
+                sheet[coord] = value
 
     workbook.save(filename=report_path)
     return report_path
--- src/api/reports/stats.py
+++ src/api/reports/stats.py
@@ -1,28 +1,43 @@
+from django.db.models import Count, Sum
+from django.db.models.expressions import RawSQL
+
 from api.models import FragmentStats, RestorationStats, SCLStats, SurveyStats
 
 
-def _calc_summary(data):
-    num_landscapes = 0
-    habitat_area = 0
-    protected_area = 0
+def _calc_summary(queryset, date_field):
 
-    for rec in data:
-        num_landscapes += 1
-        habitat_area += rec.area
-        biome_areas = rec.biome_areas or []
-        protected_area += sum([ba.get("protected") or 0 for ba in biome_areas])
-
-    try:
-        percent_protected_area = float(protected_area) / float(habitat_area)
-    except (TypeError, ValueError, ZeroDivisionError):
-        percent_protected_area = None
-
-    return dict(
-        num_landscapes=num_landscapes,
-        habitat_area=habitat_area,
-        protected_area=protected_area,
-        percent_protected_area=percent_protected_area,
+    data = list(
+        queryset.values(date_field).annotate(
+            num_landscapes=Count(date_field),
+            habitat_area=Sum("area"),
+            protected_area=Sum(
+                RawSQL(
+                    """(
+                SELECT sum(protected)
+                FROM
+                (
+                    SELECT COALESCE(((jsonb_array_elements(biome_areas)::jsonb)->'protected'), 0.0)::numeric as protected
+                ) AS foo)""",
+                    params=[],
+                )
+            ),
+        )
     )
+
+    formatted_data = dict()
+    for i in range(len(data)):
+        try:
+            percent_protected_area = data[i].get("protected_area") / data[i].get(
+                "habitat_area"
+            )
+        except (TypeError, ValueError, ZeroDivisionError):
+            percent_protected_area = None
+
+        data[i]["percent_protected_area"] = round(percent_protected_area, 6)
+        date_str = str(data[i][date_field])
+        formatted_data[date_str] = data[i]
+
+    return formatted_data
 
 
 def calc_landscape_stats(country, date, species):
@@ -39,38 +54,38 @@ def calc_landscape_stats(country, date, species):
 
 
 def calc_conservation_landscape_stats(country, date, species):
-    filter_args = dict(scl__date=date, scl__species_id=species, country=country)
+    filter_args = dict(scl__date__lte=date, scl__species_id=species, country=country)
 
     qs = SCLStats.objects.filter(**filter_args)
-    return _calc_summary(qs)
+    return _calc_summary(qs, date_field="scl__date")
 
 
 def calc_fragment_landscape_stats(country, date, species):
     filter_args = dict(
-        fragment__date=date, fragment__species_id=species, country=country
+        fragment__date__lte=date, fragment__species_id=species, country=country
     )
 
     qs = FragmentStats.objects.filter(**filter_args)
-    return _calc_summary(qs)
+    return _calc_summary(qs, date_field="fragment__date")
 
 
 def calc_restoration_landscape_stats(country, date, species):
     filter_args = dict(
-        restoration_landscape__date=date,
+        restoration_landscape__date__lte=date,
         restoration_landscape__species_id=species,
         country=country,
     )
 
     qs = RestorationStats.objects.filter(**filter_args)
-    return _calc_summary(qs)
+    return _calc_summary(qs, date_field="restoration_landscape__date")
 
 
 def calc_survey_landscape_stats(country, date, species):
     filter_args = dict(
-        survey_landscape__date=date,
+        survey_landscape__date__lte=date,
         survey_landscape__species_id=species,
         country=country,
     )
 
     qs = SurveyStats.objects.filter(**filter_args)
-    return _calc_summary(qs)
+    return _calc_summary(qs, date_field="survey_landscape__date")
