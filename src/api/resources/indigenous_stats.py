5d1dc410507ebbf0e18ffabd97af88ef206aa75a
Kim Fisher <kfisher@wcs.org>
2022-04-03 16:13:02 -0400

    indigenous range stats endpoint

---
+++ src/api/resources/indigenous_stats.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .base import BaseAPIFilterSet, StatsSerializer, StatsViewSet
+from ..models import IndigenousRangeCountryStats
+
+
+class IndigenousRangeStatsSerializer(StatsSerializer):
+    species = serializers.SerializerMethodField()
+
+    class Meta(StatsSerializer.Meta):
+        model = IndigenousRangeCountryStats
+        exclude = []
+
+    @staticmethod
+    def get_species(obj):
+        return str(obj.species)
+
+
+class IndigenousRangeStatsFilterSet(BaseAPIFilterSet):
+    class Meta:
+        model = IndigenousRangeCountryStats
+        fields = ["country", "species"]
+
+
+class IndigenousRangeStatsViewSet(StatsViewSet):
+    serializer_class = IndigenousRangeStatsSerializer
+    filter_class = IndigenousRangeStatsFilterSet
+    ordering_fields = ["species", "country"]
+
+    def get_queryset(self):
+        return IndigenousRangeCountryStats.objects.select_related()
--- src/api/urls.py
+++ src/api/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from rest_framework import routers
 
-from .resources.species import SpeciesViewSet
-from .resources.scl_stats import SCLStatsViewSet
+from .reports.views import SpeciesReportView
 from .resources.fragment_stats import RestorationFragmentStatsViewSet, SpeciesFragmentStatsViewSet, SurveyFragmentStatsViewSet
+from .resources.indigenous_stats import IndigenousRangeStatsViewSet
 from .resources.restorationls_stats import RestorationStatsViewSet
+from .resources.scl_stats import SCLStatsViewSet
+from .resources.species import SpeciesViewSet
 from .resources.surveyls_stats import SurveyStatsViewSet
 from .resources.tileviews import hii_tiles_purple, hii_tiles_rainbow
-from .reports.views import SpeciesReportView
 
 router = routers.DefaultRouter()
 
 
+router.register(r"indigenousrangestats", IndigenousRangeStatsViewSet, "indigenousrangestats")
 router.register(r"species", SpeciesViewSet, "species")
 router.register(r"restorationls_stats", RestorationStatsViewSet, "restorationls_stats")
 router.register(r"sclstats", SCLStatsViewSet, "sclstats")
